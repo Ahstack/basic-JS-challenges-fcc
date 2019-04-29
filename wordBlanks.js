b211fcc0d6ea2aa7a4728d90b54fe8afcbf3ef29
Mesfin Tegegne <mesfint@Mesfins-MBP.home>
2019-04-29 17:43:57 +0300

    word blank task completed

---
+++ wordBlanks.js
@@ -0,0 +1,21 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result =
+    myNoun +
+    " is the " +
+    myAdjective +
+    " city in northern hemisphere so that need " +
+    myVerb +
+    " " +
+    " clothes to live " +
+    myAdverb +
+    ".";
+  //'
+  //myNoun + ' is the' + myAdjective + 'city in northern hemisphere so that need' +myVerb+  'clothes to live' +  myAdverb
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("Helsinki", "coldest", "warm", "healthy");
